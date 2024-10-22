20d3dcc717c665430d4b4914f602f3674ab32863
BradG13531 <bradgale6237@gmail.com>
2024-10-22 00:04:32 -0500

    Important thd fix

--- ee_automation/thd.py
+++ ee_automation/thd.py
@@ -1,5 +1,6 @@
 import math
 
+
 def thd(values: list[float]) -> tuple[float, list[float]]:
     """Calculates the total harmonic distortion
 
@@ -10,50 +11,22 @@ def thd(values: list[float]) -> tuple[float, list[float]]:
         tuple[float, list[float]]: A tuple with the first element returning the total harmonic distortion as a percentage, and the second value returning a list containing the v_rms values of each harmonic in volts
     """
 
-    for i in range(len(values)):
-        values[i] = 10**(values[i] / 20)
-        
+    # Converts each harmonic to a V_rms value
+    values = [10 ** (value / 20) for value in values]
+
     sum = 0
     for i in range(1, len(values)):
-        sum += values[i]**2
+        sum += values[i] ** 2
 
     numerator = math.sqrt(sum)
-    thd = (numerator / values[0]) * 1000
+
+    thd = (numerator / values[0]) * 100
 
     return thd, values
 
 
-# values = []
-# i = 0
-# userInput = ''
-# print("Enter amplitude values of harmonics in dBV starting from f_0 (Enter anything except a number to calculate):")
-# while (userInput != "x"):
-#     try:
-#         userInput = float(input(f"f{i}: "))
-#         values.append(userInput)
-#         i += 1
-#     except ValueError:
-#         break
-    
-# for i in range(len(values)):
-#     values[i] = 10**(values[i] / 20)
-     
-# sum = 0
-# for i in range(1, len(values)):
-#     sum += values[i]**2
-
-# numerator = math.sqrt(sum)
-# thd = (numerator / values[0]) * 1000
-
-# print(f"\nVoltages in V_rms:")
-# for i in range(len(values)):
-#     print(f"v{i}_rms: {values[i]} V")
-
-# print("\nTotal Harmonic Distortion:")
-# print(f"{thd}%")
-
 if __name__ == "__main__":
-    list = [1, 2, 3, 4, 5]
+    list = [5, 4, 3, 2, 1]
     thd, values = thd(list)
     print(values)
     print(f"thd: {thd}%")
